8273dbb4b5b2e152deda4a1481b2547a50f90a68
Mithun Technologies <devopstrainingblr@gmail.com>
2018-11-24 06:51:41 +0530

    Updated ..

--- src/com/mt/general/HelloWorld.java
+++ src/com/mt/general/HelloWorld.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Welcome to ANT !! " + getLocalCurrentDate());
+		System.out.println("Hello Welcome to ANT !! Today Date is:  " + getLocalCurrentDate());
 
 		/*
 		 * String testCase;
